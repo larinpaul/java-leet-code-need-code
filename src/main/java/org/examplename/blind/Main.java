5b89d59454fecafb2de9b7cc9ee1816248fdaa06
larinpaul <www.larinpavel@mail.ru>
2024-08-17 09:54:45 +0500

    Duplicate integer Easy Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
    public static boolean hasDuplicateHashSet(int[] nums) {
        HashSet<Integer> set = new HashSet<>();
        for (int num : nums) {
            if (!set.add(num)) {
                return true;
            }
        }
        return false;
    }

---
+++ src/main/java/org/examplename/blind/Main.java
@@ -0,0 +1,13 @@
+package org.examplename.blind;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello! :)");
+
+        System.out.println("This is a set of tasks from the 75 blind interview questions! :D");
+
+    }
+
+}
