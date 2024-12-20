b990e810fca450aa410f53ee0571aa93eb83132f
larinpaul <www.larinpavel@mail.ru>
2024-12-20 07:36:47 +0500

    Started solving Group Anagram
    class Solution {
        public List<List<String>> groupAnagram(String[] strs) {
    
            return List.of(List.of("Hi1", "Hi2"));
        }
    }

---
+++ src/main/java/org/examplename/blind/AAarraysandhashing/GroupAnagram.java
@@ -0,0 +1,18 @@
+package org.examplename.blind.AAarraysandhashing;
+
+import java.util.List;
+
+public class GroupAnagram {
+
+    public static void main(String[] args) {
+
+    }
+
+}
+
+class GroupAnagramSolution {
+    public List<List<String>> groupAnagram(String[] strs) {
+
+        return List.of(List.of("Hi1", "Hi2"));
+    }
+}
