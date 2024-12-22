783ade26cd3dc1bd225d055ab1fafd27bdcaa134
larinpaul <www.larinpavel@mail.ru>
2024-12-22 20:40:39 +0500

    Started solving Encode and Decode Strings. Medium
    package org.examplename.blind.AAarraysandhashing;
    
    import java.util.Arrays;
    import java.util.List;
    
    public class EncodeAndDecodeString {
    
        // https://neetcode.io/problems/string-encode-and-decode
    
        // Encode and Decode Strings
    
        // Medium
    
        // Design an algorithm to encode a list of strings to a single string.
        // The encoded string is then decoded back to the original list of strings.
    
        // Please implement encode and decode
    
        // Example 1:
        // Input: ["neet","code","love","you"]
        // Output: ["neet","code","love","you"]
    
        // Example 2:
        // Input: ["we","say",":","yes"]
        // Output: ["we","say",":","yes"]
    
        // Constraints:
        // * 0 <= strs.length < 100
        // * 0 <= strs[i].length < 200
        // * strs[i] contains only UTF-8 characters.
    
        // Recommended Time & Space Complexity
        // You should aim for a solution with O(m) time and O(1) space
        // for each encode() and decode() call,
        // where m is th sum of lengths of all the strings.
    
        // Hint 1
        // A naive solution would be to use a non-ascii character as a delimiter.
        // Can you think of a better way?
    
        // Hint 2
        // Try to encode and decode the strings using a smart approach
        // based on the lengths of each string.
        // How can you differentiate between the lengths
        // and any numbers that might be present in the strings?
    
        // Hint 3
        // We can use an encoding approach where we start with a number
        // representing the length of the string, followed by a separator
        // character (let's use # for simplicity), and then the string itself.
        // To decode, we read the number until we reach a #.
        // then use that number
        // to read the specified number of characters as the string.
    
    }
    
    class SoltuionEcodeAndDecodeStrings  {
    
        public String encode(List<String> strs) {
    
            return "Hello";
        }
    
        public List<String> decode(String str) {
    
            return Arrays.asList("You can do it! :)");
        }
    
    }

---
+++ src/main/java/org/examplename/blind/AAarraysandhashing/EncodeAndDecodeString.java
@@ -0,0 +1,69 @@
+package org.examplename.blind.AAarraysandhashing;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class EncodeAndDecodeString {
+
+    // https://neetcode.io/problems/string-encode-and-decode
+
+    // Encode and Decode Strings
+
+    // Medium
+
+    // Design an algorithm to encode a list of strings to a single string.
+    // The encoded string is then decoded back to the original list of strings.
+
+    // Please implement encode and decode
+
+    // Example 1:
+    // Input: ["neet","code","love","you"]
+    // Output: ["neet","code","love","you"]
+
+    // Example 2:
+    // Input: ["we","say",":","yes"]
+    // Output: ["we","say",":","yes"]
+
+    // Constraints:
+    // * 0 <= strs.length < 100
+    // * 0 <= strs[i].length < 200
+    // * strs[i] contains only UTF-8 characters.
+
+    // Recommended Time & Space Complexity
+    // You should aim for a solution with O(m) time and O(1) space
+    // for each encode() and decode() call,
+    // where m is th sum of lengths of all the strings.
+
+    // Hint 1
+    // A naive solution would be to use a non-ascii character as a delimiter.
+    // Can you think of a better way?
+
+    // Hint 2
+    // Try to encode and decode the strings using a smart approach
+    // based on the lengths of each string.
+    // How can you differentiate between the lengths
+    // and any numbers that might be present in the strings?
+
+    // Hint 3
+    // We can use an encoding approach where we start with a number
+    // representing the length of the string, followed by a separator
+    // character (let's use # for simplicity), and then the string itself.
+    // To decode, we read the number until we reach a #.
+    // then use that number
+    // to read the specified number of characters as the string.
+
+}
+
+class SoltuionEcodeAndDecodeStrings  {
+
+    public String encode(List<String> strs) {
+
+        return "Hello";
+    }
+
+    public List<String> decode(String str) {
+
+        return Arrays.asList("You can do it! :)");
+    }
+
+}
