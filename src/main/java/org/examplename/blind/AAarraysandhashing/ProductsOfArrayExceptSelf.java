0b47d8b122d975726acbc2b5fc15c53809079d26
larinpaul <www.larinpavel@mail.ru>
2024-12-29 10:34:04 +0500

    // * Time complexity: O(n^2)     // * Space complexity: O(1) since the output array is excluded from space analysis.     public int[] productExceptSelfBruteForce(int[] nums) {         int n = nums.length;         int[] res = new int[n];
            for (int i = 0; i < n; i++) {
                int prod = 1;
                for (int j = 0; j < n; j++) {
                    if (i != j) {
                        prod *= nums[j];
                    }
                }
                res[i] = prod;
            }
            return res;
        }

--- src/main/java/org/examplename/blind/AAarraysandhashing/ProductsOfArrayExceptSelf.java
+++ src/main/java/org/examplename/blind/AAarraysandhashing/ProductsOfArrayExceptSelf.java
@@ -67,4 +67,23 @@ class SolutionProductsOfArrayExceptSelf {
         return integerArray;
     }
 
+    // * Time complexity: O(n^2)
+    // * Space complexity: O(1) since the output array is excluded from space analysis.
+    public int[] productExceptSelfBruteForce(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int prod = 1;
+            for (int j = 0; j < n; j++) {
+                if (i != j) {
+                    prod *= nums[j];
+                }
+            }
+            res[i] = prod;
+        }
+        return res;
+    }
+
+
 }
